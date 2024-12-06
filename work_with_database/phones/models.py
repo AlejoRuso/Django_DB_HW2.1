fe811fe1f42fa1cee7e215c3420ab3c4af398c08
Alejo <istominalexey@ro.ru>
2024-12-06 12:45:04 +0300

    realised

--- work_with_database/phones/models.py
+++ work_with_database/phones/models.py
@@ -2,5 +2,10 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+            #id = models.CharField(max_length=50, primary_key = True)
+            name = models.CharField(max_length=100)
+            price = models.DecimalField(max_digits=50, decimal_places=2)
+            image = models.CharField()
+            release_date = models.DateField(auto_now=False, auto_now_add=False)
+            lte_exists = models.BooleanField(null=False)
+            slug = models.CharField(max_length = 80, unique=True)
--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -8,10 +8,24 @@ def index(request):
 def show_catalog(request):
     template = 'catalog.html'
     context = {}
+    sort_by = request.GET.get('sort', 'name')
+    if sort_by == 'min_price':
+        all_phones = Phone.objects.order_by('price')
+    elif sort_by == 'max_price':
+        all_phones = Phone.objects.order_by('-price')
+    elif sort_by == 'name':
+        all_phones = Phone.objects.order_by('name')
+    else:
+        all_phones = Phone.objects.order_by('id')
+    context.setdefault('phones', all_phones)
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
     context = {}
+    context = {
+    'phone':
+    Phone.objects.filter(slug__contains = slug).first()
+    }
     return render(request, template, context)
